6b503a375ee5db9778c9e5a0f86f4a0144acdfe0
Kota Oshiro <g.g.gaaaaaaaame@gmail.com>
2023-07-30 01:44:11 +0900

    微調整

--- projects/api/mysite/wsgi.py
+++ projects/api/mysite/wsgi.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 from django.core.wsgi import get_wsgi_application
 
