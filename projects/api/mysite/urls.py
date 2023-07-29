8de1839534894eb4ef7b26ffa75b7bb4e8b77e37
Kota Oshiro <g.g.gaaaaaaaame@gmail.com>
2023-07-30 01:52:52 +0900

    微調整

--- projects/api/mysite/urls.py
+++ projects/api/mysite/urls.py
@@ -4,7 +4,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mysite.urls')),
+    path('', include('render.urls')),
 ]
 
 if settings.DEBUG:
