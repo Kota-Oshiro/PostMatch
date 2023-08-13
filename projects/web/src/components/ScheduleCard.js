058d30ed77b2baab7de91ff529871d10ff614dee
Kota Oshiro <g.g.gaaaaaaaame@gmail.com>
2023-08-14 03:17:58 +0900

    userページのwatchリストではscoreのinvisibleをしないように分岐処理

--- projects/web/src/components/ScheduleCard.js
+++ projects/web/src/components/ScheduleCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { formatUsing, formats } from '../DateFormat.js';
 import './ScheduleCard.css';
 
@@ -7,6 +7,12 @@ import { ReactComponent as PostIcon } from '../icons/post.svg';
 import { ReactComponent as WatchedIconGrey } from '../icons/watched_grey.svg';
 
 function ScheduleCard({ match, isScoreVisible }) {
+
+    const location = useLocation();
+
+    const renderMatchScore = location.pathname.includes("/schedule") 
+        ? renderMatchScoreForSchedule 
+        : renderMatchScoreForWatch;
 
     return (
         <Link to={`/match/${match.id}`} className='match-links'>
@@ -18,7 +24,7 @@ function ScheduleCard({ match, isScoreVisible }) {
                 alt={match.home_team.tla}
                 className='schedule-crest'
                 />
-                {renderMatchScoreOrTime(match, isScoreVisible)}
+                {renderMatchScore(match, isScoreVisible)}
                 <img
                 src={`https://res.cloudinary.com/dx5utqv2s/image/upload/v1686214597/Crest/crest-${match.away_team.tla}.webp`}
                 alt={match.away_team.tla}
@@ -44,7 +50,7 @@ function ScheduleCard({ match, isScoreVisible }) {
     );
 }
 
-function renderMatchScoreOrTime(match, isScoreVisible) {
+function renderMatchScoreForSchedule(match, isScoreVisible) {
     const isRecent = isWithin7Days(match.started_at);
     const { status, home_score, away_score } = match;
 
@@ -60,6 +66,33 @@ function renderMatchScoreOrTime(match, isScoreVisible) {
         } else if (status === 'PAUSED' || status === 'IN_PLAY') {
             return <span className={`schedule-text schedule-invisible ${status === 'IN_PLAY' ? 'schedule-inplay' : ''}`}>試合中</span>;
         }
+    }
+
+    if (status === 'TIMED') {
+        return <span className='schedule-text schedule-timed'>{formatUsing(match.started_at, formats.HOUR_MINUTE)}</span>;
+    }
+
+    return <span className='schedule-text schedule-timed'>-- : --</span>;
+}
+
+// started_atが7日以内か判定する用
+function isWithin7Days(dateTimeStr) {
+    const now = new Date(); // 現在の日付と時刻を取得
+    const matchDate = new Date(dateTimeStr); // 引数の日付文字列をDateオブジェクトに変換
+    const difference = now - matchDate; // 両者の差をミリ秒で取得
+
+    const oneDayInMilliseconds = 24 * 60 * 60 * 1000; // 1日のミリ秒
+    const sevenDaysInMilliseconds = oneDayInMilliseconds * 7; // 7日のミリ秒
+
+    return difference <= sevenDaysInMilliseconds; // 7日以内であるか判定
+}
+
+
+function renderMatchScoreForWatch(match) {
+    const { status, home_score, away_score } = match;
+
+    if (status === 'FINISHED' || status === 'PAUSED' || status === 'IN_PLAY') {
+        return <span className={`schedule-text ${status === 'IN_PLAY' ? 'schedule-inplay' : ''}`}>{home_score} - {away_score}</span>;
     }
 
     if (status === 'TIMED') {
@@ -92,16 +125,5 @@ function renderMatchDateTime(match) {
     }
   }
 
-// started_atが7日以内か判定する用
-function isWithin7Days(dateTimeStr) {
-    const now = new Date(); // 現在の日付と時刻を取得
-    const matchDate = new Date(dateTimeStr); // 引数の日付文字列をDateオブジェクトに変換
-    const difference = now - matchDate; // 両者の差をミリ秒で取得
-
-    const oneDayInMilliseconds = 24 * 60 * 60 * 1000; // 1日のミリ秒
-    const sevenDaysInMilliseconds = oneDayInMilliseconds * 7; // 7日のミリ秒
-
-    return difference <= sevenDaysInMilliseconds; // 7日以内であるか判定
-}
 
 export default ScheduleCard;
