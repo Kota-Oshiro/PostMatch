abc1e8e216716e73b6604d1d62cd5416feb0d852
Kota Oshiro <g.g.gaaaaaaaame@gmail.com>
2023-08-30 03:43:54 +0900

    goalsデータの手動更新で一時的にview変更

--- projects/api/myapp/management/commands/fetch_goals.py
+++ projects/api/myapp/management/commands/fetch_goals.py
@@ -1,25 +1,18 @@
 from django.core.management.base import BaseCommand
-from myapp.views import fetch_recent_match_goals
-# from myapp.views import fetch_goals_data
+#from myapp.views import fetch_recent_match_goals
+from myapp.views import fetch_goals_data
 
 
-class Command(BaseCommand):
-    help = 'fetch_recent_match_goals'
-
-    def handle(self, *args, **options):
-        fetch_recent_match_goals()
-
-''' match_id指定でgoalsをfetchしたいとき用
 
 class Command(BaseCommand):
     help = 'fetch_goals_data'
 
     def handle(self, *args, **options):
         # 指定されたIDのリストを作成
-        match_ids = [438488,438487]
+        match_ids = [438500,444269,444272,438494,444273,438497,444264,438502]
 
        # 各IDに対して関数を実行
         for match_id in match_ids:
             fetch_goals_data(match_id)
 
-'''
+
